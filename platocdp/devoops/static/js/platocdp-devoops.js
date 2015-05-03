338c44d77ff2a90a775a3734ec22de54eb2c35c9
Izhar Firdaus <izhar@kagesenshi.org>
2015-05-03 16:52:28 +0800

    add custom css and js for theme

---
+++ platocdp/devoops/static/js/platocdp-devoops.js
@@ -0,0 +1,449 @@
+//
+//    Main script of DevOOPS v1.0 Bootstrap Theme
+//
+"use strict";
+//
+// Dynamically load  OpenStreetMap Plugin
+// homepage: http://openlayers.org
+//
+function LoadOpenLayersScript(callback){
+	if (!$.fn.OpenLayers){
+		$.getScript('http://www.openlayers.org/api/OpenLayers.js', callback);
+	}
+	else {
+		if (callback && typeof(callback) === "function") {
+			callback();
+		}
+	}
+}
+//
+//  Function maked all .box selector is draggable, to disable for concrete element add class .no-drop
+//
+function WinMove(){
+	$( "div.box").not('.no-drop')
+		.draggable({
+			revert: true,
+			zIndex: 2000,
+			cursor: "crosshair",
+			handle: '.box-name',
+			opacity: 0.8
+		})
+		.droppable({
+			tolerance: 'pointer',
+			drop: function( event, ui ) {
+				var draggable = ui.draggable;
+				var droppable = $(this);
+				var dragPos = draggable.position();
+				var dropPos = droppable.position();
+				draggable.swap(droppable);
+				setTimeout(function() {
+					var dropmap = droppable.find('[id^=map-]');
+					var dragmap = draggable.find('[id^=map-]');
+					if (dragmap.length > 0 || dropmap.length > 0){
+						dragmap.resize();
+						dropmap.resize();
+					}
+					else {
+						draggable.resize();
+						droppable.resize();
+					}
+				}, 50);
+				setTimeout(function() {
+					draggable.find('[id^=map-]').resize();
+					droppable.find('[id^=map-]').resize();
+				}, 250);
+			}
+		});
+}
+//
+// Swap 2 elements on page. Used by WinMove function
+//
+jQuery.fn.swap = function(b){
+	b = jQuery(b)[0];
+	var a = this[0];
+	var t = a.parentNode.insertBefore(document.createTextNode(''), a);
+	b.parentNode.insertBefore(a, b);
+	t.parentNode.insertBefore(b, t);
+	t.parentNode.removeChild(t);
+	return this;
+};
+//
+// Create OpenLayers map with required options and return map as object
+//
+function drawMap(lon, lat, elem, layers) {
+	var LayersArray = [];
+	// Map initialization
+	var map = new OpenLayers.Map(elem);
+	// Add layers on map
+	map.addLayers(layers);
+	// WGS 1984 projection
+	var epsg4326 =  new OpenLayers.Projection("EPSG:4326");
+	//The map projection (Spherical Mercator)
+	var projectTo = map.getProjectionObject();
+	// Max zoom = 17
+	var zoom=10;
+	map.zoomToMaxExtent();
+	// Set longitude/latitude
+	var lonlat = new OpenLayers.LonLat(lon, lat);
+	map.setCenter(lonlat.transform(epsg4326, projectTo), zoom);
+	var layerGuest = new OpenLayers.Layer.Vector("You are here");
+	// Define markers as "features" of the vector layer:
+	var guestMarker = new OpenLayers.Feature.Vector(
+		new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo)
+	);
+	layerGuest.addFeatures(guestMarker);
+	LayersArray.push(layerGuest);
+	map.addLayers(LayersArray);
+	// If map layers > 1 then show checker
+	if (layers.length > 1){
+		map.addControl(new OpenLayers.Control.LayerSwitcher({'ascending':true}));
+	}
+	// Link to current position
+	map.addControl(new OpenLayers.Control.Permalink());
+	// Show current mouse coords
+	map.addControl(new OpenLayers.Control.MousePosition({ displayProjection: epsg4326 }));
+	return map
+}
+//
+//  Function for create 2 dates in human-readable format (with leading zero)
+//
+function PrettyDates(){
+	var currDate = new Date();
+	var year = currDate.getFullYear();
+	var month = currDate.getMonth() + 1;
+	var startmonth = 1;
+	if (month > 3){
+		startmonth = month -2;
+	}
+	if (startmonth <=9){
+		startmonth = '0'+startmonth;
+	}
+	if (month <= 9) {
+		month = '0'+month;
+	}
+	var day= currDate.getDate();
+	if (day <= 9) {
+		day = '0'+day;
+	}
+	var startdate = year +'-'+ startmonth +'-01';
+	var enddate = year +'-'+ month +'-'+ day;
+	return [startdate, enddate];
+}
+//
+//  Function set min-height of window (required for this theme)
+//
+function SetMinBlockHeight(elem){
+	elem.css('min-height', window.innerHeight - 49)
+}
+//
+//  Helper for correct size of Messages page
+//
+function MessagesMenuWidth(){
+	var W = window.innerWidth;
+	var W_menu = $('#sidebar-left').outerWidth();
+	var w_messages = (W-W_menu)*16.666666666666664/100;
+	$('#messages-menu').width(w_messages);
+}
+//
+//  Beauty tables plugin (navigation in tables with inputs in cell)
+//  Created by DevOOPS.
+//
+(function( $ ){
+	$.fn.beautyTables = function() {
+		var table = this;
+		var string_fill = false;
+		this.on('keydown', function(event) {
+		var target = event.target;
+		var tr = $(target).closest("tr");
+		var col = $(target).closest("td");
+		if (target.tagName.toUpperCase() == 'INPUT'){
+			if (event.shiftKey === true){
+				switch(event.keyCode) {
+					case 37: // left arrow
+						col.prev().children("input[type=text]").focus();
+						break;
+					case 39: // right arrow
+						col.next().children("input[type=text]").focus();
+						break;
+					case 40: // down arrow
+						if (string_fill==false){
+							tr.next().find('td:eq('+col.index()+') input[type=text]').focus();
+						}
+						break;
+					case 38: // up arrow
+						if (string_fill==false){
+							tr.prev().find('td:eq('+col.index()+') input[type=text]').focus();
+						}
+						break;
+				}
+			}
+			if (event.ctrlKey === true){
+				switch(event.keyCode) {
+					case 37: // left arrow
+						tr.find('td:eq(1)').find("input[type=text]").focus();
+						break;
+					case 39: // right arrow
+						tr.find('td:last-child').find("input[type=text]").focus();
+						break;
+				case 40: // down arrow
+					if (string_fill==false){
+						table.find('tr:last-child td:eq('+col.index()+') input[type=text]').focus();
+					}
+					break;
+				case 38: // up arrow
+					if (string_fill==false){
+						table.find('tr:eq(1) td:eq('+col.index()+') input[type=text]').focus();
+					}
+						break;
+				}
+			}
+			if (event.keyCode == 13 || event.keyCode == 9 ) {
+				event.preventDefault();
+				col.next().find("input[type=text]").focus();
+			}
+			if (string_fill==false){
+				if (event.keyCode == 34) {
+					event.preventDefault();
+					table.find('tr:last-child td:last-child').find("input[type=text]").focus();}
+				if (event.keyCode == 33) {
+					event.preventDefault();
+					table.find('tr:eq(1) td:eq(1)').find("input[type=text]").focus();}
+			}
+		}
+		});
+		table.find("input[type=text]").each(function(){
+			$(this).on('blur', function(event){
+			var target = event.target;
+			var col = $(target).parents("td");
+			if(table.find("input[name=string-fill]").prop("checked")==true) {
+				col.nextAll().find("input[type=text]").each(function() {
+					$(this).val($(target).val());
+				});
+			}
+		});
+	})
+};
+})( jQuery );
+//
+// Beauty Hover Plugin (backlight row and col when cell in mouseover)
+//
+//
+(function( $ ){
+	$.fn.beautyHover = function() {
+		var table = this;
+		table.on('mouseover','td', function() {
+			var idx = $(this).index();
+			var rows = $(this).closest('table').find('tr');
+			rows.each(function(){
+				$(this).find('td:eq('+idx+')').addClass('beauty-hover');
+			});
+		})
+		.on('mouseleave','td', function(e) {
+			var idx = $(this).index();
+			var rows = $(this).closest('table').find('tr');
+			rows.each(function(){
+				$(this).find('td:eq('+idx+')').removeClass('beauty-hover');
+			});
+		});
+	};
+})( jQuery );
+//
+//  Function convert values of inputs in table to JSON data
+//
+//
+function Table2Json(table) {
+	var result = {};
+	table.find("tr").each(function () {
+		var oneRow = [];
+		var varname = $(this).index();
+		$("td", this).each(function (index) { if (index != 0) {oneRow.push($("input", this).val());}});
+		result[varname] = oneRow;
+	});
+	var result_json = JSON.stringify(result);
+	OpenModalBox('Table to JSON values', result_json);
+}
+/*-------------------------------------------
+	Function for File upload page (form_file_uploader.html)
+---------------------------------------------*/
+function FileUpload(){
+	$('#bootstrapped-fine-uploader').fineUploader({
+		template: 'qq-template-bootstrap',
+		classes: {
+			success: 'alert alert-success',
+			fail: 'alert alert-error'
+		},
+		thumbnails: {
+			placeholders: {
+				waitingPath: "assets/waiting-generic.png",
+				notAvailablePath: "assets/not_available-generic.png"
+			}
+		},
+		request: {
+			endpoint: 'server/handleUploads'
+		},
+		validation: {
+			allowedExtensions: ['jpeg', 'jpg', 'gif', 'png']
+		}
+	});
+}
+/*-------------------------------------------
+	Function for OpenStreetMap page (maps.html)
+---------------------------------------------*/
+//
+// Load GeoIP JSON data and draw 3 maps
+//
+function LoadTestMap(){
+	$.getJSON("http://www.telize.com/geoip?callback=?",
+		function(json) {
+			var osmap = new OpenLayers.Layer.OSM("OpenStreetMap");//создание слоя карты
+			var googlestreets = new OpenLayers.Layer.Google("Google Streets", {numZoomLevels: 22,visibility: false});
+			var googlesattelite = new OpenLayers.Layer.Google( "Google Sattelite", {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22});
+			var map1_layers = [googlestreets,osmap, googlesattelite];
+			// Create map in element with ID - map-1
+			var map1 = drawMap(json.longitude, json.latitude, "map-1", map1_layers);
+			$("#map-1").resize(function(){ setTimeout(map1.updateSize(), 500); });
+			// Create map in element with ID - map-2
+			var osmap1 = new OpenLayers.Layer.OSM("OpenStreetMap");//создание слоя карты
+			var map2_layers = [osmap1];
+			var map2 = drawMap(json.longitude, json.latitude, "map-2", map2_layers);
+			$("#map-2").resize(function(){ setTimeout(map2.updateSize(), 500); });
+			// Create map in element with ID - map-3
+			var sattelite = new OpenLayers.Layer.Google( "Google Sattelite", {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22});
+			var map3_layers = [sattelite];
+			var map3 = drawMap(json.longitude, json.latitude, "map-3", map3_layers);
+			$("#map-3").resize(function(){ setTimeout(map3.updateSize(), 500); });
+		}
+	);
+}
+/*-------------------------------------------
+	Function for Fullscreen Map page (map_fullscreen.html)
+---------------------------------------------*/
+//
+// Create Fullscreen Map
+//
+function FullScreenMap(){
+	$.getJSON("http://www.telize.com/geoip?callback=?",
+		function(json) {
+			var osmap = new OpenLayers.Layer.OSM("OpenStreetMap");//создание слоя карты
+			var googlestreets = new OpenLayers.Layer.Google("Google Streets", {numZoomLevels: 22,visibility: false});
+			var googlesattelite = new OpenLayers.Layer.Google( "Google Sattelite", {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22});
+			var map1_layers = [googlestreets,osmap, googlesattelite];
+			var map_fs = drawMap(json.longitude, json.latitude, "full-map", map1_layers);
+		}
+	);
+}
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//
+//      MAIN DOCUMENT READY SCRIPT OF DEVOOPS THEME
+//
+//      In this script main logic of theme
+//
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+$(document).ready(function () {
+	$('.show-sidebar').on('click', function (e) {
+		e.preventDefault();
+		$('div#main').toggleClass('sidebar-show');
+		setTimeout(MessagesMenuWidth, 250);
+	});
+	$('.main-menu').on('click', 'a', function (e) {
+		var parents = $(this).parents('li');
+		var li = $(this).closest('li.dropdown');
+		var another_items = $('.main-menu li').not(parents);
+		another_items.find('a').removeClass('active');
+		another_items.find('a').removeClass('active-parent');
+		if ($(this).hasClass('dropdown-toggle') || $(this).closest('li').find('ul').length == 0) {
+			$(this).addClass('active-parent');
+			var current = $(this).next();
+			if (current.is(':visible')) {
+				li.find("ul.dropdown-menu").slideUp('fast');
+				li.find("ul.dropdown-menu a").removeClass('active')
+			}
+			else {
+				another_items.find("ul.dropdown-menu").slideUp('fast');
+				current.slideDown('fast');
+			}
+		}
+		else {
+			if (li.find('a.dropdown-toggle').hasClass('active-parent')) {
+				var pre = $(this).closest('ul.dropdown-menu');
+				pre.find("li.dropdown").not($(this).closest('li')).find('ul.dropdown-menu').slideUp('fast');
+			}
+		}
+		if ($(this).hasClass('active') == false) {
+			$(this).parents("ul.dropdown-menu").find('a').removeClass('active');
+			$(this).addClass('active')
+		}
+		if ($(this).attr('href') == '#') {
+			e.preventDefault();
+		}
+	});
+	var height = window.innerHeight - 49;
+	$('#main').css('min-height', height)
+		.on('click', '.expand-link', function (e) {
+			var body = $('body');
+			e.preventDefault();
+			var box = $(this).closest('div.box');
+			var button = $(this).find('i');
+			button.toggleClass('fa-expand').toggleClass('fa-compress');
+			box.toggleClass('expanded');
+			body.toggleClass('body-expanded');
+			var timeout = 0;
+			if (body.hasClass('body-expanded')) {
+				timeout = 100;
+			}
+			setTimeout(function () {
+				box.toggleClass('expanded-padding');
+			}, timeout);
+			setTimeout(function () {
+				box.resize();
+				box.find('[id^=map-]').resize();
+			}, timeout + 50);
+		})
+		.on('click', '.collapse-link', function (e) {
+			e.preventDefault();
+			var box = $(this).closest('div.box');
+			var button = $(this).find('i');
+			var content = box.find('div.box-content');
+			content.slideToggle('fast');
+			button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+			setTimeout(function () {
+				box.resize();
+				box.find('[id^=map-]').resize();
+			}, 50);
+		})
+		.on('click', '.close-link', function (e) {
+			e.preventDefault();
+			var content = $(this).closest('div.box');
+			content.remove();
+		});
+	$('body').on('click', 'a.close-link', function(e){
+		e.preventDefault();
+		CloseModalBox();
+	});
+	$('#top-panel').on('click','a', function(e){
+		if ($(this).hasClass('ajax-link')) {
+			e.preventDefault();
+			if ($(this).hasClass('add-full')) {
+				$('#content').addClass('full-content');
+			}
+			else {
+				$('#content').removeClass('full-content');
+			}
+			var url = $(this).attr('href');
+			window.location = url;
+		}
+	});
+	$('#search').on('keydown', function(e){
+		if (e.keyCode == 13){
+			e.preventDefault();
+			$('#content').removeClass('full-content');
+			ajax_url = 'ajax/page_search.html';
+			window.location = ajax_url;
+		}
+	});
+
+});
+
+
